df8ea293f50c6f0bb24cc08842c471c9e7572689
toyolife881 <toyolife.island881@gmail.com>
2022-06-11 23:52:57 +0900

    リクエストを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,8 +8,14 @@ class PostsController < ApplicationController
   # end
 
   def create
-    Post.create(content: params[:content])
-    # indexアクションにリダイレクト
-    redirect_to action: :index
+    # Post.create(content: params[:content])
+    # # indexアクションにリダイレクト
+    # redirect_to action: :index
+
+    post = Post.create(content: params[:content])
+    # renderメソッドで、レスポンスで返却されるデータフォーマットにJSONを指定
+    # jsonオプションで、{ post: post}というデータをJSON形式で返却
+    # { post: post}は、{ キー: 変数postの値}
+    render json:{ post: post}
   end
 end
